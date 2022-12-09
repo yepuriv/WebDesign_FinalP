4b371ceaf1188dcea64c1ecd7d9e0044dc6f6a1a
Akshit <kallepalli.l@northeastern.edu>
2022-12-09 07:07:13 -0500

    Added cart functions and updated few products

--- server/data/Products.js
+++ server/data/Products.js
@@ -1,74 +1,184 @@
 const products = [
+ 
   {
-    name: "Table 1",
-    image: "/images/6.png",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-    price: 89,
-    countInStock: 3,
-    rating: 4,
-    numReviews: 4,
-  },
-  {
-    name: "Table 2",
-    image: "/images/5.png",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-    price: 599,
-    countInStock: 10,
-    rating: 2,
-    numReviews: 2,
-  },
-  {
-    name: "Toilet cupboard 1",
+    name: "Sporty bathroom storage unit",
     image: "/images/4.png",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-    price: 929,
+      "Simple stylish design, Functional and suitable for any room.",
+    price: 40,
     countInStock: 0,
     rating: 3.5,
     numReviews: 3,
   },
   {
-    name: "Toilet cupboard 2",
+    name: "Sporty bathroom storage unit",
     image: "/images/3.png",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-    price: 399,
+      "Simple stylish design, Functional and suitable for any room.",
+    price: 40,
     countInStock: 10,
     rating: 5,
     numReviews: 9,
   },
   {
-    name: "Sofa 1",
-    image: "/images/2.png",
+    name: "Wood dining table",
+    image: "/images/5.png",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      "Simple stylish design, Functional and suitable for any room.",
     price: 49,
     countInStock: 7,
     rating: 2,
     numReviews: 2,
   },
   {
-    name: "Sofa 2",
-    image: "/images/1.png",
+    name: "All purpose table",
+    image: "/images/6.png",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      "Simple stylish design, Functional and suitable for any room.",
     price: 29,
-    countInStock: 0,
-    rating: 0,
-    numReviews: 0,
+    countInStock: 1,
+    rating: 1,
+    numReviews: 1,
   },
   {
-    name: "Sofa 3",
-    image: "/images/1.png",
+    name: "Budget table",
+    image: "/images/D1.png",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-    price: 29,
-    countInStock: 0,
-    rating: 0,
-    numReviews: 0,
+      "Simple stylish design, Functional and suitable for any room.",
+    price: 30,
+    countInStock: 50,
+    rating: 5,
+    numReviews: 1000,
   },
-];
+  {
+    name: "Originals dining table",
+    image: "/images/D2.png",
+    description:
+      "Simple stylish design, Functional and suitable for any room.",
+    price: 60,
+    countInStock: 76,
+    rating: 4,
+    numReviews: 66,
+  },
+  {
+    name: "Simple dining table",
+    image: "/images/D3.png",
+    description:
+      "Simple stylish design, Functional and suitable for any room.",
+    price: 50,
+    countInStock: 50,
+    rating: 5,
+    numReviews: 55,
+  },
+  {
+    name: "Family dining table",
+    image: "/images/D4.png",
+    description:
+      "Simple stylish design, Functional and suitable for any room.",
+    price: 80,
+    countInStock: 80,
+    rating: 4,
+    numReviews: 90,
+  },
+  {
+    name: "Sofa de royal",
+    image: "/images/S1.png",
+    description:
+      "Simple stylish design, Functional and suitable for any room.",
+    price: 35,
+    countInStock: 5,
+    rating: 5,
+    numReviews: 400,
+  },
+  {
+    name: "General Sofa",
+    image: "/images/S2.png",
+    description:
+      "This sofa couch will complete your home. It not only works functionally but adds exquisiteness to your space and creates a home feeling and elegant vibes. Ergonomically designed, a leisure sofa brings a place to get more comfort while watching videos, playing games, reading books, or relaxing in front of the fireplace. Performs well in the living room, apartment, reception room, and office.",
+    price: 30,
+    countInStock: 33,
+    rating: 3,
+    numReviews: 133,
+  },
+  {
+    name: "Royal Sofas",
+    image: "/images/S3.png",
+    description:
+      "This sofa couch will complete your home. It not only works functionally but adds exquisiteness to your space and creates a home feeling and elegant vibes. Ergonomically designed, a leisure sofa brings a place to get more comfort while watching videos, playing games, reading books, or relaxing in front of the fireplace. Performs well in the living room, apartment, reception room, and office.",
+    price: 70,
+    countInStock: 100,
+    rating: 4,
+    numReviews: 44,
+  },
+  {
+    name: "Family sofa",
+    image: "/images/S4.png",
+    description:
+      "This sofa couch will complete your home. It not only works functionally but adds exquisiteness to your space and creates a home feeling and elegant vibes. Ergonomically designed, a leisure sofa brings a place to get more comfort while watching videos, playing games, reading books, or relaxing in front of the fireplace. Performs well in the living room, apartment, reception room, and office.",
+    price: 50,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 77,
+  },
+  
+  {
+    name: "Moonlight Sofa",
+    image: "/images/1.png",
+    description: "This sofa couch will complete your home. It not only works functionally but adds exquisiteness to your space and creates a home feeling and elegant vibes. Ergonomically designed, a leisure sofa brings a place to get more comfort while watching videos, playing games, reading books, or relaxing in front of the fireplace. Performs well in the living room, apartment, reception room, and office.",
+    price: 50,
+    countInStock: 3,
+    rating: 40,
+  },
+  {
+    name: "Lightview Sofa",
+    image: "/images/2.png",
+    description:
+      "This sofa couch will complete your home. It not only works functionally but adds exquisiteness to your space and creates a home feeling and elegant vibes. Ergonomically designed, a leisure sofa brings a place to get more comfort while watching videos, playing games, reading books, or relaxing in front of the fireplace. Performs well in the living room, apartment, reception room, and office.",
+    price: 60,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 20,
+  },
+  {
+    name: "Royal bed",
+    image: "/images/B1.png",
+    description:
+      "Simple stylish design, Functional and suitable for any room.",
+    price: 100,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 300,
+  },
 
+  {
+    name: "Beige bed",
+    image: "/images/B2.png",
+    description:
+      "Simple stylish design, Functional and suitable for any room.",
+    price: 70,
+    countInStock: 18,
+    rating: 4.5,
+    numReviews: 298,
+  },
+  {
+    name: "Comfort bed",
+    image: "/images/B3.png",
+    description:
+      "Simple stylish design, Functional and suitable for any room.",
+    price: 95,
+    countInStock: 100,
+    rating: 3.75,
+    numReviews: 33,
+  },
+  {
+    name: "Hinged Bed",
+    image: "/images/B4.png",
+    description:
+      "Simple stylish design, Functional and suitable for any room.",
+    price: 120,
+    countInStock: 21,
+    rating: 5,
+    numReviews: 100,
+  },
+]
 export default products;
